82df3f22c569edb0ff353a33e27bcf9c7bc3d1b9
SanlarisDev <hello@sanlaris.dev>
2023-03-26 14:59:02 +0200

    39 Colocar css

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 import { FirstApp } from "./FirstApp";
-import { HelloWorldApp } from "./HelloWorldApp";
+// import { HelloWorldApp } from "./HelloWorldApp";
+import './style.css';
 
 
 ReactDom.createRoot(document.getElementById('root'))
