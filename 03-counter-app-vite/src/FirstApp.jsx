764345799f2de32e4b8dd0fabf2e084d736dabf6
SanlarisDev <hello@sanlaris.dev>
2023-03-26 14:54:39 +0200

    38 Variables en html

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
+const newMessage = {
+  message: 'Hola Mundo',
+  title: 'Fernando'
+};
+
+const getResult = (a, b) => a+b;
+
 export const FirstApp = ( ) => {
-    return (
-      <h1>Cynthia</h1>
-    );
-  }
+  return (
+    <>
+      <h1>{ newMessage.message }</h1>
+      {/* <code>{ JSON.stringify(newMessage) }</code> */}
+      <h3>Suma: { getResult(1,2) }</h3>
+      <p>Soy un subtítulo</p>
+    </>
+  );
+}
